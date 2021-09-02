8c1b4e754d5ae957a593db5cb88095c2bc237d6b
Himadri Bhargava <himadribhargava@gmail.com>
2021-09-02 22:46:45 +0530

    show quote and tweet quote on button click

--- script.js
+++ script.js
@@ -1,2 +1,66 @@
-window.console.log("testing")
- console.log(window)
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// show new Quote
+function newQuote() {
+    showLoadingSpinner();
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    
+    // check if author field is blank then show unknown 
+    authorText.textContent = quote.author ? quote.author: "Unknown";
+
+    // check quote length to determine styling 
+    if(quote.text.length > 50){
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
+    quoteText.textContent = quote.text;
+    removeLoadingSpinner();
+};
+
+// Get quotes from API
+async function getQuotes(){
+    showLoadingSpinner();
+  const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        
+    }
+};
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on load
+// newQuote(); 
+
+getQuotes();
+
+// loading();
